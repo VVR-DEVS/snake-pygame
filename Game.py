79545ec407fdc2c42db2c722162a5ffc500d2ba0
Wercton <werctonmatheus@gmail.com>
2021-01-31 00:57:44 -0300

    spieler created

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from Snake import Snake
 from Settings import *
 
 
@@ -11,6 +12,7 @@ class Spiel:
         self.clock = pg.time.Clock()
         pg.display.set_caption(TITLE)
         self.running = True
+        self.jogador = Snake()
 
     def neueSpiel(self):
         self.run()
@@ -34,14 +36,19 @@ class Spiel:
                 self.running = False
 
     def draw(self):
-        self.bildschirm.fill(GREY)
+        self.bildschirm.fill(BLACK)
         self.draw_grid()
+        self.draw_snake()
         pg.display.flip()
         
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.bildschirm, BLACK, (x, 0), (x, HEIGHT))
-            pg.draw.line(self.bildschirm, BLACK, (0, x), (WIDTH, x))
+            pg.draw.line(self.bildschirm, GREY, (x, 0), (x, HEIGHT))
+            pg.draw.line(self.bildschirm, GREY, (0, x), (WIDTH, x))
+
+    def draw_snake(self):
+        for pos in self.jogador.snake:
+            self.bildschirm.blit(self.jogador.snake_skin, pos)
 
     def startbildschirm(self):
         pass
