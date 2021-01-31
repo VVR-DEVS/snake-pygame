5b22c30e4c2722a43500cff68d528eb2eb120ac8
MateusRosario <mateusdasilvarusario@gmail.com>
2021-01-31 02:00:18 -0300

    updating changing direction

--- Snake.py
+++ Snake.py
@@ -20,21 +20,22 @@ class Snake:
         for i in range(len(self.snake) - 1, 0, -1):
             self.snake[i] = (self.snake[i-1][0], self.snake[i-1][1])
 
-        if self.direction == self.left:
+        if self.direction == 'left':
             self.snake[0] = (self.snake[0][0] - TILESIZE, self.snake[0][1])
-        elif self.direction == self.right:
+        elif self.direction == 'right':
             self.snake[0] = (self.snake[0][0] + TILESIZE, self.snake[0][1])
-        elif self.direction == self.up:
+        elif self.direction == 'up':
             self.snake[0] = (self.snake[0][0], self.snake[0][1] - TILESIZE)
         else:
             self.snake[0] = (self.snake[0][0], self.snake[0][1] + TILESIZE)
 
     def change_direction(self, move):
-        if move == 'left':
-            self.direction = self.left
-        elif move == 'right':
-            self.direction = self.right
-        elif move == 'up':
-            self.direction = self.up
-        else:
-            self.direction = self.down
+        if self.direction == 'up' and move is 'down':
+            return
+        if self.direction == 'down' and move is 'up':
+            return
+        if self.direction == 'right' and move is 'left':
+            return
+        if  self.direction == 'left' and move is 'right':
+            return
+        self.direction = move
