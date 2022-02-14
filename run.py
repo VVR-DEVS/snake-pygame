15bf15fd0426d2d4a74b682f0a44d2ec64049ce1
MateusRosario <mateusdasilvarusario@gmail.com>
2022-02-13 21:50:40 -0300

    improvement: run by command adjusted

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 from client.spiel import *
+from server.server import start_server
+import sys
 
-g = Spiel()
-g.startbildschirm()
+print(sys.argv)
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'runserver':
+        start_server()
+    else:
+        raise Exception('Argumento inválido recebido')
+else:
+    g = Spiel()
+    g.startbildschirm()
 
-while g.running:
-    g.neueSpiel()
+    while g.running:
+        g.neueSpiel()
 
-pg.quit()
-
-
-
+    pg.quit()
